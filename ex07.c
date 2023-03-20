d5ce99932b3cb9531368e329f86544b0c7466bb6
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-20 16:12:58 +0100

    Add files via upload

---
+++ ex07.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int x;
+int y;
+printf("inserisci due numeri ");
+scanf("%d %d", &x, &y );
+if (x%y==0)
+{
+
+
+printf("x è multilpo di y");
+
+
+
+}
+
+else
+{
+
+printf("x non è multiplo di y");
+
+}
+
+
+}
