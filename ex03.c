4e402c1264299e8b736a1e9446246073e42262ae
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-20 14:52:10 +0100

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,31 @@
+//inserisci due numeri e farne la somma. 
+#include <stdio.h>
+int main()
+{
+int x;
+int y; 
+
+printf("inserisci due numeri ");
+scanf("%d %d", &x, &y );
+int z = x+y;
+printf("somma: %d" , z);
+
+int d = x-y;
+printf("differenza: %d" ,d);
+
+int e = x * y; 
+printf("moltiplicazione: %d" ,e);
+
+int f = x/y;
+printf("divisione: %d" ,f );
+
+
+
+
+
+
+
+
+
+}
+
