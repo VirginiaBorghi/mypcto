23521b5e78ae64ca00ca0d2de1b71b1f9dfdb792
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-20 15:33:47 +0100

    Add files via upload

---
+++ ex05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int x;
+printf("inserisci un età  ");
+scanf("%d", &x);
+
+if (x>18)
+{
+printf("sei maggiorenne ");
+
+}
+
+else
+{
+printf("sei minorenne");
+
+}
+
+
+}
