e85dc3ff2f200abe67a4bf13b879f8264339caf0
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-20 15:23:13 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,34 @@
+// il programma legge un intero
+//incrementa di 7 e stampa, dimunuisce di 7 e stampa, moltiplica di 7 e stampa, divide di 7 e stampa
+
+#include <stdio.h>
+int main()
+{
+int x;
+
+printf("inserisci un numero  ");
+scanf("%d", &x);
+x = x +7;
+printf(" nuova variabile : %d" , x);
+x = x -7;
+printf(" nuova variabile : %d" , x);
+x = x *7;
+printf(" nuova variabile : %d" , x);
+x = x/7;
+printf(" nuova variabile : %d" , x);
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+ 
