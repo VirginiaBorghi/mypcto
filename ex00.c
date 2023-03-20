07281e9005d5a97dcf6f89f3a2a10d8498436de5
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-20 12:38:33 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+float x = 32;
+
+char y =  'B';
+char z = 'C';
+int c = 109;
+
+printf("ho creato una variabile float %f, il primo carattere %c è uguale al secondo carattere %c, ultimo int %c\n", x ,y, z,c);
+
+}
+
+
+
