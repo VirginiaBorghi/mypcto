4a5ba33e7cb1b01191b32102cee8dfe3eb34311d
VirginiaBorghi <128369811+VirginiaBorghi@users.noreply.github.com>
2023-03-21 15:25:34 +0100

    Add files via upload

---
+++ ex18.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+
+{
+int x=0;
+while(x<100)
+{
+  if (x<10)
+  {
+  
+ printf(" 0%d", x);  
+  }
+  else 
+  {
+
+ printf(" %d", x);  
+  } 
+  x=x+1;
+  
+
+}
+  
+
+
+}
+
